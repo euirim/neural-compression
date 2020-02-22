f5daf3f3752c21eb842ac58565481d7627717834
Max Sobol Mark <maxsobolmark@gmail.com>
2020-02-22 12:46:39 -0800

    Finished first version of the protocol

--- LMProtocol.py
+++ LMProtocol.py
@@ -2,19 +2,22 @@ from .ILanguageModel import ILanguageModel
 from collections import OrderedDict
 import math
 from bitarray import bitarray
+import zlib
 
 class LMProtocol:
 
-	def __init__(self, language_model, context_window_length=16, next_word_possibilities_number=16, out_of_vocabulary_word_max_bit_size=1024):
+	def __init__(self, language_model, context_window_length=16, next_word_possibilities_number=16, out_of_vocabulary_word_max_bit_size=1024, initial_context_max_bit_size=16384):
 		"""
 		@param language_model: ILanguageModel
 		"""
 		assert math.log2(next_word_possibilities_number).is_integer()
 		assert math.log2(out_of_vocabulary_word_max_bit_size).is_integer()
+		assert math.log2(initial_context_max_bit_size).is_integer()
 		self.lm = language_model(context_window_length=16, next_word_possibilities_number=16)
 		self._context_window_length = context_window_length
 		self._next_word_possibilities_number = next_word_possibilities_number
 		self._out_of_vocabulary_word_max_bit_size = out_of_vocabulary_word_max_bit_size
+		self._initial_context_max_bit_size = initial_context_max_bit_size
 
 
 	def compress(self, text):
@@ -24,6 +27,21 @@ class LMProtocol:
 		"""
 
 		compressed_object = self._get_compressed_object(text)
+		binary = self._get_binary_from_object(compressed_object)
+		zlib_binary = zlib.compress(binary.tobytes())
+
+		return zlib_binary
+
+	def decompress(self, compressed_binary):
+		"""
+		@param compressed_binary: binary string
+		@returns original string
+		"""
+		binary = zlib.decompress(compressed_binary)
+		compressed_object = self._get_object_from_binary(binary)
+		uncompressed_string = self._get_string_from_compressed_object(compressed_object)
+		return uncompressed_string
+
 
 	def _get_compressed_object(self, text):
 		"""
@@ -57,7 +75,9 @@ class LMProtocol:
 	def _get_binary_from_object(self, compressed_object):
 		binary = bitarray()
 		binary_initial_context = bitarray()
-		binary_initial_context.frombytes(compressed_object['initial_context'].encode('utf-8'))
+		initial_context_bytes = compressed_object['initial_context'].encode('utf-8')
+		binary_initial_context.extend(('{0:0' + str(math.log2(self._initial_context_max_bit_size)) + 'b}').format(str(len(initial_context_bytes))))
+		binary_initial_context.extend(initial_context_bytes)
 		binary.extend(binary_initial_context)
 
 		for word in compressed_object['words']:
@@ -104,8 +124,33 @@ class LMProtocol:
 
 	def _get_ranking_from_probabilities(self, word_probabilities, word):
 		"""
-		@param word_probabilities: Dict<word, probability>.
+		@param word_probabilities: Dict<word, probability>
 		@returns ranking: Int
 		"""
 		ordered_words = OrderedDict(sorted(word_probabilities.items(), key=lambda t: -t[1]))
 		return list(ordered_words.keys()).index(word)
+
+	def _get_object_from_binary(self, binary):
+		obj = {}
+		initial_context_length_binary = binary[0:int(math.log2(self._initial_context_max_bit_size))]
+		initial_context_length = int(initial_context_length_binary.to01(), 2)
+		initial_context_binary = binary[int(math.log2(self._initial_context_max_bit_size)):int(math.log2(self._initial_context_max_bit_size)) + initial_context_length]
+		obj['initial_context'] = initial_context_binary.decode('utf-8')
+		obj['words'] = []
+		words_binary = binary[int(math.log2(self._initial_context_max_bit_size)) + initial_context_length:]
+		while len(words_binary) > 0:
+			word = {}
+			word['out_of_vocabulary'] = words_binary[0]
+			if word['out_of_vocabulary']:
+				word_binary_length_binary = words_binary[1 : 1 + int(math.log2(self._out_of_vocabulary_word_max_bit_size))]
+				word_binary_length = int(word_binary_length_binary.to01(), 2)
+				word_binary = words_binary[1 + int(math.log2(self._out_of_vocabulary_word_max_bit_size)) : 1 + int(math.log2(self._out_of_vocabulary_word_max_bit_size)) + word_binary_length]
+				word['word'] = word_binary.decode('utf-8')
+				words_binary = words_binary[1 + int(math.log2(self._out_of_vocabulary_word_max_bit_size)) + word_binary_length :]
+			else:
+				ranking_binary = words_binary[1 : 1 + int(math.log2(self._next_word_possibilities_number))]
+				word['ranking'] = int(ranking_binary.to01(), 2)
+				words_binary = words_binary[1 + int(math.log2(self._next_word_possibilities_number)) :]
+			obj['words'].append(word)
+		
+		return obj
